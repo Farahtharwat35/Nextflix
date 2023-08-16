869e3129fd0e528acf0c53e55ed053426377eb30
silentunicorn249 <markbassem@hotmail.com>
2023-08-17 01:15:37 +0300

    updated configuration data

--- Backend Nestjs/src/main.ts
+++ Backend Nestjs/src/main.ts
@@ -5,10 +5,9 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-  .setTitle('Cats example')
-  .setDescription('The cats API description')
+  .setTitle('Nextflix')
   .setVersion('1.0')
-  .addTag('cats')
+  .addTag('Nextflix')
   .build();
 const document = SwaggerModule.createDocument(app, config);
 SwaggerModule.setup('api', app, document);
