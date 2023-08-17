2a51a3ff3d6e801c48a2ad8aa1be3dee579ba4b8
silentunicorn249 <markbassem@hotmail.com>
2023-08-17 16:13:28 +0300

    fixed app not able to run

--- Backend Nestjs/src/App/app.module.ts
+++ Backend Nestjs/src/App/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from 'src/User/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/nextflix')],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/nextflix'), 
+  UserModule
+],
 })
 export class AppModule {}
