d10f68f215f992bcbf5fa43a46c7e3c27545bf82
silentunicorn249 <markbassem@hotmail.com>
2023-08-17 20:44:02 +0300

    Added ValidationPipe

--- Backend Nestjs/src/main.ts
+++ Backend Nestjs/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './App/app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe())
   const config = new DocumentBuilder()
   .setTitle('Nextflix')
   .setVersion('1.0')
@@ -11,6 +13,6 @@ async function bootstrap() {
   .build();
 const document = SwaggerModule.createDocument(app, config);
 SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
