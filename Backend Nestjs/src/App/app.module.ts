36a437b0125b2e5a0794395040cd0eb40a1a2963
silentunicorn249 <markbassem@hotmail.com>
2023-08-18 21:53:23 +0300

    Added modules

--- Backend Nestjs/src/App/app.module.ts
+++ Backend Nestjs/src/App/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from 'src/User/user.module';
 import { AccountModule } from 'src/Account/account.module';
+import { SubscriptionModule } from 'src/Subscription/subscription.module';
+import { CategoryModule } from 'src/Category/category.module';
 
 @Module({
   // imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/nextflix')
@@ -12,7 +14,9 @@ import { AccountModule } from 'src/Account/account.module';
       // 'mongodb://192.168.146.14:27017/nextflix'),
       'mongodb+srv://nextflix:mongodb1234@cluster0.gbumfm1.mongodb.net/?retryWrites=true&w=majority'), // for my remote db (comment it and uncomment the above line)
     UserModule, 
-    AccountModule
+    AccountModule,
+    SubscriptionModule,
+    CategoryModule
   ]
 })
 export class AppModule { }
