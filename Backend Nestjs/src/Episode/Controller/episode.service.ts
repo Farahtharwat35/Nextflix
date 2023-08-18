20e561e63a68413285c0eeca17a859ace7b638ab
Farahtharwat35 <farahtharwat35@gmail.com>
2023-08-18 20:49:05 +0300

    episode modified

--- Backend Nestjs/src/Episode/Controller/episode.service.ts
+++ Backend Nestjs/src/Episode/Controller/episode.service.ts
@@ -1,21 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Episode } from '../Model/episode.schema';
 
 @Injectable()
 export class EpisodeService {
-  constructor(
-    @InjectModel(Episode.name) private episodeModel: Model<Episode>,
-  ) {}
+  constructor(@InjectModel(Episode.name) private episodeModel: Model<Episode>) {}
 
   async createEpisode(episodeData: Partial<Episode>): Promise<Episode> {
-    const createdEpisode = new this.episodeModel(episodeData);
-    return createdEpisode.save();
+    const newEpisode = new this.episodeModel(episodeData);
+    return newEpisode.save();
   }
 
-  async findAllEpisodes(): Promise<Episode[]> {
+  async getAllEpisodes(): Promise<Episode[]> {
     return this.episodeModel.find().exec();
   }
 
+  async getEpisodeById(id: string): Promise<Episode> {
+    const episode = await this.episodeModel.findById(id).exec();
+    if (!episode) {
+      throw new NotFoundException('Episode not found');
+    }
+    return episode;
+  }
+
+  async updateEpisode(id: string, episodeData: Partial<Episode>): Promise<Episode> {
+    const updatedEpisode = await this.episodeModel.findByIdAndUpdate(id, episodeData, { new: true }).exec();
+    if (!updatedEpisode) {
+      throw new NotFoundException('Episode not found');
+    }
+    return updatedEpisode;
+  }
+
+  async deleteEpisode(id: string): Promise<void> {
+    const result = await this.episodeModel.findByIdAndDelete(id).exec();
+    if (!result) {
+      throw new NotFoundException('Episode not found');
+    }
+  }
 }
