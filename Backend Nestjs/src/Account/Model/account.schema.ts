029f80e959264bf57d9c2c09e38c25c6af414542
silentunicorn249 <markbassem@hotmail.com>
2023-08-18 20:54:58 +0300

    Update account.schema.ts

--- Backend Nestjs/src/Account/Model/account.schema.ts
+++ Backend Nestjs/src/Account/Model/account.schema.ts
@@ -1,6 +1,8 @@
 import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsArray, IsEmail, IsNotEmpty, IsString, ValidateNested } from 'class-validator';
 import { HydratedDocument } from 'mongoose';
+import { Subscription } from 'rxjs';
 import { User } from 'src/User/Model/user.schema';
 
 export type AccountDocument = HydratedDocument<Account>;
@@ -8,22 +10,34 @@ export type AccountDocument = HydratedDocument<Account>;
 export class Account {
     @Prop()
     @IsNotEmpty()
+    @IsString()
     name: string;
+
     @Prop()
     @IsNotEmpty()
+    @IsString()
     email: string
+
     @Prop()
     @IsNotEmpty()
+    @IsArray()
+    @ValidateNested({each: true})
+    @Type(() => User)
     users: User[]
+
     @Prop()
     @IsNotEmpty()
     password: string
+
     @Prop()
     @IsNotEmpty()
     pohoneNo: string
+
     @Prop()
     @IsNotEmpty()
-    subscription: "Normal" | "Platinum"
+    @ValidateNested()
+    @Type(() => Subscription)
+    subscription: Subscription
 
     
 
