ff16bbce77958f58eae97a198ca0c1325f7c4ea6
EtshD1 <mohamed.h.abouelenin@gmail.com>
2023-08-21 01:23:29 +0300

    Navbar: Catergory link

--- ui/src/components/navbar.tsx
+++ ui/src/components/navbar.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 const Navbar = () => {
     return (
         <nav className="fixed top-0 left-20 right-0 h-16 bg-slate-950 flex gap-4 items-center">
-			<h2 className="font-bold text-2xl text-indigo-700">Nextflix</h2>
+            <div>
+                <h2 className="font-bold text-2xl text-indigo-700">Nextflix</h2>
+            </div>
+            <div className="grow flex">
+                <Link href="/categories">Categories</Link>
+            </div>
         </nav>
     );
 };
