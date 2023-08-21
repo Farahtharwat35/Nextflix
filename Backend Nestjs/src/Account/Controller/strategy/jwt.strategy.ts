9ac2c22fc41f55d424d42492c5b9f9a7ff7a7aea
Farahtharwat35 <farahtharwat35@gmail.com>
2023-08-21 21:43:01 +0300

    commit

--- Backend Nestjs/src/Account/Controller/strategy/jwt.strategy.ts
+++ Backend Nestjs/src/Account/Controller/strategy/jwt.strategy.ts
@@ -2,7 +2,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { JwtPayload } from '../interface/jwtPayload.interface';
-import { jwtConstants } from '../account.const';
+import { jwtConstants } from '../../account.const';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
--- Backend Nestjs/src/User/Controller/user.controller.ts
+++ Backend Nestjs/src/User/Controller/user.controller.ts
@@ -3,20 +3,21 @@ import { User } from '../Model/user.schema';
 import { RolesGuard } from '../../Middlewares/roles.guard';
 import { Roles } from '../../Middlewares/roles.decorator'; 
 import { UserService } from './user.service';
+import { JwtStrategy } from 'src/Account/Controller/strategy/jwt.strategy';
 
 @Controller('Users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  @UseGuards(RolesGuard)
+  @UseGuards(RolesGuard,JwtStrategy)
   @Roles('Admin')
   async createUser(@Body() userData: User): Promise<User> {
     return this.userService.create(userData);
   }
 
   @Get()
-  @UseGuards(RolesGuard) 
+  @UseGuards(RolesGuard,JwtStrategy) 
   @Roles('Admin', 'Watcher') 
   async getAllUsers(): Promise<User[]> {
     return this.userService.findAll();
